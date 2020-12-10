d938d51af9b1bae8e1365f98e68da3a69c9e904c
Rafael B <rafaborgui@gmail.com>
2020-12-10 00:26:42 -0300

    form cadastro local

--- site/src/reservas/local-reservavel/localReservavel.ctrl.js
+++ site/src/reservas/local-reservavel/localReservavel.ctrl.js
@@ -4,4 +4,10 @@ angular.module('ReservasModule').controller('LocalReservavelCtrl',
         HeaderFactory.setHeader('reservas', 'Locais reserváveis');
         let user = JSON.parse(localStorage.getItem("bioacs-uid"));
         AuthService.aclPaginaService($state.$current.name, user.id).then(result => $scope.accessPagina = result.data);
+
+        $scope.createNovoLocal = function () {
+            $scope.step = 1;
+            $('#cadastroLocal').modal('show');
+        }
+
     });

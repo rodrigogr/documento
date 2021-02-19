73f7c87abdc2a512ba12f2fc987beac72d3a554c
Jaderson <jadersonaires@gmail.com>
2021-02-19 10:07:30 -0300

    imoveis remove codigo não utilizado

--- api/app/Models/Imovel.php
+++ api/app/Models/Imovel.php
@@ -34,45 +34,7 @@ class Imovel extends Model
         return ($id != NULL) ? self::where('id', $id)->with('imoveis_permanentes')->first() : self::with('imoveis_permanentes')->get();
     }
 
-    static public function getTotalImovelAtivoNaoFicticio()
-    {
-        return Imovel::all()->where('imovel_ficticio', '=', '0')->count();
-    }
-
-    static public function getAreaTotalCondominio()
-    {
-        return Imovel::all()->where('imovel_ficticio', '=', '0')->sum('base_calculo');
-    }
     
-    public function getAreaImovelAttribute($value)
-    {
-        return DataHelper::getFloat2Real($value);
-    }
-
-    public function setAreaImovelAttribute($value)
-    {
-        return $this->attributes['area_imovel'] = DataHelper::getReal2Float($value);
-    }
-
-    public function getAreaConstruidaAttribute($value)
-    {
-        return DataHelper::getFloat2Real($value);
-    }
-
-    public function setAreaConstruidaAttribute($value)
-    {
-        return $this->attributes['area_construida'] = DataHelper::getReal2Float($value);
-    }
-
-    public function getAreaAjardinadaAttribute($value)
-    {
-        return DataHelper::getFloat2Real($value);
-    }
-
-    public function setAreaAjardinadaAttribute($value)
-    {
-        return $this->attributes['area_ajardinada'] = DataHelper::getReal2Float($value);
-    }
 
     // ******************** RELASHIONSHIP ******************************
     // ************************** belongsTo ****************************
@@ -86,43 +48,11 @@ class Imovel extends Model
         return $this->belongsTo('App\Models\SituacaoImovel', 'idsituacao_imovel');
     }
 
-    public function grupo_calculo_imovel()
-    {
-        return$this->belongsTo('App\Models\GrupoCalculoImovel');
-    }
+
     // ************************** hasMany ******************************
-    public function areas_externas()
-    {
-        return $this->hasMany('App\Models\AreaExterna', 'idimovel');
-    }
-
-    public function documentos()
-    {
-        return $this->hasMany('App\Models\AreaExterna', 'idimovel');
-    }
-
+    
     public function imoveis_permanentes()
     {
         return $this->hasMany('App\Models\ImovelPermanente', 'id_imovel');
-    }
-
-    public function pre_lancamentos()
-    {
-        return $this->hasMany('App\Models\PreLancamento', 'idimovel');
-    }
-
-    public function receita_calculo_fundo()
-    {
-        return $this->hasMany('App\Models\ReceitaCalculoFundo', 'id_imovel');
-    }
-
-    public function receita_calculo_taxa()
-    {
-        return $this->hasMany('App\Models\ReceitaCalculoTaxa','id_imovel');
-    }
-
-    public function contas_receber()
-    {
-        return $this->hasMany('App\Models\ContasReceber', 'id_imovel');
     }
 }

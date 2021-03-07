ca5ce7973aa898088e10e3c5ea033e55ac03375c
Jaderson Aires <jadersonaires@gmail.com>
2021-03-07 17:58:36 -0300

    add cors aplication and remove typehead

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -55,6 +55,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 //        'jwt.auth' => \Tymon\JWTAuth\Middleware\GetUserFromToken::class,
         'jwt.auth' => \App\Http\Middleware\CheckToken::class,
-        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class
+        'jwt.refresh' => \Tymon\JWTAuth\Middleware\RefreshToken::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }

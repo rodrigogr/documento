d4773d768adf6777663498c40f952414702e1a84
Rafael Borges <rafaborgui@gmail.com>
2021-03-26 09:59:46 -0300

    ajuste data inativo

--- api/app/Models/Reservas/LocalReservavel.php
+++ api/app/Models/Reservas/LocalReservavel.php
@@ -35,7 +35,11 @@ class LocalReservavel extends Model
 
     public static function complete($id)
     {
-        return self::where('id', $id)->with(['localidade','periodo','diaInativo'])->first();
+        return self::where('id', $id)->with(['localidade','periodo'])
+            ->with(['diaInativo' => function ($q) {
+                $q->select('id','id_local_reservavel','descricao','repetir',\DB::raw("date_format(data,'%d/%m/%Y') as data"));
+            }])
+            ->first();
     }
 
     public static function nomeLocalReservavel($nome_local)

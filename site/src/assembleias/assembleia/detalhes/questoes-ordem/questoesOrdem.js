cec058b54a154e282bbecc93b21480fadcc05a40
Jaderson Aires <jadersonaires@gmail.com>
2021-04-12 21:14:52 -0300

    page questão de orderm modal

--- site/src/assembleias/assembleia/detalhes/questoes-ordem/questoesOrdem.js
+++ site/src/assembleias/assembleia/detalhes/questoes-ordem/questoesOrdem.js
@@ -11,7 +11,48 @@ angular.module('appDirectives').directive("assembleiaquestoes", function () {
     }
 });
 
-function assembleiaQuestoesOrdemCtrl ($scope, $state, $filter, UtilsService, config)
-{
+function assembleiaQuestoesOrdemCtrl($scope, $state, $filter, UtilsService, config) {
+
+
+    //** Modal Questão de ordem */
+    $scope.abreQuestaoOrdem = function (id) {
+        console.log(id);
+
+        $scope.selectDiscussao = [{
+                id: 1,
+                foto: '../../../../../img/avatar.png',
+                nome: 'Joaquim Almeida',
+                titulo: 'Há outras prioridades no momento, como discutido em outra assembleia Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                like: '10',
+                deslike: '2',
+                number: '5'
+            },
+            {
+                id: 5,
+                foto: '../../../../../img/avatar.png',
+                nome: 'Joaquim Almeida',
+                titulo: 'Há outras prioridades no momento, como discutido em outra assembleia Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                like: 10,
+                deslike: 2,
+                number: 5
+            },
+            {
+                id: 7,
+                foto: '../../../../../img/avatar.png',
+                nome: 'Joaquim Almeida',
+                titulo: 'Há outras prioridades no momento, como discutido em outra assembleia Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                like: 10,
+                deslike: 2,
+                number: 5
+            }
+        ];
+
+        $('#abreQuestaoOrdem').modal('show');
+    }
+
+    $scope.fechaQuestaoOrdem = function () {
+        $('#abreQuestaoOrdem').modal('hide');
+    }
+    //** Modal Questão de ordem */
 
 }

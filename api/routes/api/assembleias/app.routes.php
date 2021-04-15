f8a1125ada80e2c1764424dc68865564a0bb6521
Rafael Gonzaga Goncalves <rafael.gonzaga@uzer.com.br>
2021-04-14 23:56:23 -0300

    primeiras rotas do aplicativo

--- api/routes/api/assembleias/app.routes.php
+++ api/routes/api/assembleias/app.routes.php
@@ -1,1 +1,6 @@
 <?php
+
+Route::group(['prefix' => '5eec33a37ed64e42575b84f1abc8a27843b1a050/assembleias'], function () {
+    Route::get('/', 'assembleia\AssembleiaController@listaAssembleiasUsuario');
+    Route::get('/{id}', 'assembleia\AssembleiaController@getAssembleiaDetalhadaUsuario');
+});

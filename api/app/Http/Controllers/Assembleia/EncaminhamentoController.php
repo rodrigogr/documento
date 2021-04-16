f881b829ced33c1847d28e1d4f96d5324413a14a
Rodrigo Gonçalves <rogrebelo@gmail.com>
2021-04-16 10:53:01 -0300

    Detalhe encaminhamento.

--- api/app/Http/Controllers/Assembleia/EncaminhamentoController.php
+++ api/app/Http/Controllers/Assembleia/EncaminhamentoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Assembleia;
 
 use App\Http\Controllers\Controller;
 use App\models\Assembleia\AssembleiaEncaminhamento;
+use App\models\Assembleia\AssembleiaPauta;
 use App\models\Assembleia\AssembleiaPergunta;
 use App\models\Assembleia\AssembleiaThead;
 use App\models\Assembleia\Pauta;
@@ -56,7 +57,29 @@ class EncaminhamentoController extends Controller
     public function detalhar($idEncaminhamento)
     {
         // TODO Detalhar encaminhamento
-    }
+
+        $encaminhamento = DB::table('assembleia_encaminhamentos', 'assembleia_encaminhamentos.id', $idEncaminhamento)
+            ->get()->first();
+
+        $pautaDiscutida = AssembleiaPauta::join('assembleia_perguntas', 'assembleia_pautas.id_pergunta', 'assembleia_perguntas.id')
+            ->where('assembleia_pautas.id', $encaminhamento->id_pauta)
+            ->select('assembleia_pautas.id as id_pauta','assembleia_perguntas.pergunta')
+            ->get()->first();
+
+        $anexos = DB::table('assembleia_theads_anexos')
+        ->where('assembleia_theads_anexos.id_thead', $encaminhamento->id_thead)
+        ->select('file')
+        ->get();
+
+        $pautaDiscutida['encaminhamento'] = Assembleiathead::join('bioacesso_portaria.pessoa', 'assembleia_theads.id_pessoa', 'pessoa.id')
+        ->where('assembleia_theads.id', $encaminhamento->id_thead)
+        ->select( 'pessoa.nome as autor', 'pessoa.url_foto as ulr_foto_autor', 'assembleia_theads.titulo')
+        ->get();
+
+        $pautaDiscutida['encaminhamento']['anexos'] = $anexos;
+
+        return response()->success($pautaDiscutida);
+   }
 
     /*
      *  Salva uma reposta do encaminhamento

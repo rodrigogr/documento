7faab8004e335a5f9e07d11ecb27f63f01ed0800
Paulo Vitor <paulovitornn@gmail.com>
2021-04-23 01:55:08 -0300

    implementacao da integracao e views das pautas.

--- site/src/assembleias/assembleia/detalhes/pautas/pautas.js
+++ site/src/assembleias/assembleia/detalhes/pautas/pautas.js
@@ -15,151 +15,34 @@ function assembleiaPautasCtrl ($scope, $state, $filter, UtilsService, config, $h
 {
     $scope.resumoPautas = [];
     $scope.detalhesPautas = [];
-    $scope.ultimaAlternativa = 1;
+    $scope.ultimaAlternativa = 0;
+    $scope.motivoSuspender = '';
+    $scope.pautaSelecao = {};
 
     function getPautasAssembleia(id = 0)
     {
-        console.log($state.params.id);
         var promisse = ($http.get(`${config.apiUrl}api/assembleias/pautas/`+$state.params.id));
         promisse.then(function (retorno) {
-            console.log(retorno);
             $scope.resumoPautas = retorno.data.data;
-            console.log($scope.resumoPautas);
         }).finally( () => {
         });
-
-
-        /*$scope.resumoPautas = [
-            {
-                idPauta:'1',
-                tituloPauta:'Qual empresa de segurança devemos contratar?',
-                totalAlternativas:'3',
-                status:'Aguardando inicio',
-                totalVotos:'0'
-            },
-            {
-                idPauta: '2',
-                tituloPauta:'Devemos mudar o serviço de jardinagem do residencial?',
-                totalAlternativas:'2',
-                status:'Aberta para votação',
-                totalVotos:'32'
-            },
-            {
-                idPauta: '3',
-                tituloPauta:'Devemos reformar o parquinho imediatamente?',
-                totalAlternativas:'2',
-                status:'Suspensa',
-                totalVotos:'19'
-            }
-        ];*/
-
-        forEach(pauta in $scope.resumoPautas)
-        {
-            getDetalhesPautas(pauta.id);
-        }
-        angular.forEach($scope.detalhesPautas,
-            function (value, key){
-                console.log (key + ' '+ value.id);
-                getDetalhesPautas(value.id);
-                console.log (key + ' '+ value.id);
-            }
-        );
     }
 
     function getDetalhesPautas(id_pauta)
     {
-        $scope.detalhesPautas = '';
+        $scope.pautaSelecao = {};
         var promisse = ($http.get(`${config.apiUrl}api/pautas/`+id_pauta));
         promisse.then(function (retorno) {
-            console.log(retorno);
-            $scope.detalhesPautas = retorno.data.data;
-            console.log($scope.detalhesPautas);
+            $scope.pautaSelecao = retorno.data.data[0];
         }).finally( () => {
+            $scope.ultimaAlternativa = $scope.pautaSelecao.alternativas.length;
         });
-        /*$scope.detalhesPautas = [
-            {
-                idPauta:'1',
-                descricaoPergunta:'Qual empresa de segurança devemos contratar?',
-                alternativas:[
-                    {
-                        idAlternativa:'1',
-                        descAlternativa:'Sim, eu concordo.'
-                    },
-                    {
-                        idAlternativa:'2',
-                        descAlternativa:'Não, eu discordo.'
-                    },
-                    {
-                        idAlternativa:'3',
-                        descAlternativa:'Não concordo nem discordo, muito pelo contrário.'
-                    },
-                ],
-                anexos:[
-                    {
-                        idAnexo:'1',
-                        anexo:'anexo-1.jpg'
-                    },
-                    {
-                        idAnexo:'2',
-                        anexo:'anexo-2.jpg'
-                    },
-                ]
-            },
-            {
-                idPauta:'2',
-                descricaoPergunta:'Devemos mudar o serviço de jardinagem do residencial?',
-                alternativas:[
-                    {
-                        idAlternativa:'1',
-                        descAlternativa:'Sim, eu concordo.'
-                    },
-                    {
-                        idAlternativa:'2',
-                        descAlternativa:'Não, eu discordo.'
-                    }
-                ],
-                anexos:[]
-            },
-            {
-                idPauta:'3',
-                descricaoPergunta:'Devemos reformar o parquinho imediatamente?',
-                alternativas:[
-                    {
-                        idAlternativa:'1',
-                        descAlternativa:'Sim, eu concordo.'
-                    },
-                    {
-                        idAlternativa:'2',
-                        descAlternativa:'Não, eu discordo.'
-                    }
-                ],
-                anexos:[]
-            }
-        ]*/
-        console.log($scope.detalhesPautas);
     }
 
-    $scope.pautaSelecao = {};
     getPautasAssembleia();
 
     $scope.abrePauta = function (idPauta){
         getDetalhesPautas(idPauta);
-        console.log(idPauta);
-        angular.forEach($scope.detalhesPautas,
-            function (value, key){
-                console.log (key + ' '+ value.id);
-                if(idPauta == value.id)
-                {
-                    console.log("x");
-                    $scope.pautaSelecao = value;
-                    console.log($scope.pautaSelecao);
-                    return true;
-                }
-                console.log (key + ' '+ value.id);
-            }
-            );
-        $scope.ultimaAlternativa = $scope.pautaSelecao.alternativas.length;
-        console.log($scope.ultimaAlternativa);
         $('#abrePauta').modal('show');
     }
 

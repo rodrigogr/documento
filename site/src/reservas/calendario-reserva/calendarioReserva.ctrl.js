10eaee8ad509b6f963f18c3da04939c48216c590
Rafael B <rafaborgui@gmail.com>
2021-05-07 00:23:19 -0300

    montando o calendario localidade pt-br - css

--- site/src/reservas/calendario-reserva/calendarioReserva.ctrl.js
+++ site/src/reservas/calendario-reserva/calendarioReserva.ctrl.js
@@ -18,72 +18,90 @@ angular.module('ReservasModule').controller('CalendarioReservaCtrl',
             .finally(() => $scope.loadLocais = true);
 
         $(document).ready( function () {
-            var calendarEl = document.getElementById('calendar');
+            setTimeout( montaCalendario, 1000);
 
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialDate: '2020-09-12',
-                editable: true,
-                selectable: true,
-                businessHours: true,
-                dayMaxEvents: true, // allow "more" link when too many events
-                events: [
-                    {
-                        title: 'All Day Event',
-                        start: '2020-09-01'
-                    },
-                    {
-                        title: 'Long Event',
-                        start: '2020-09-07',
-                        end: '2020-09-10'
-                    },
-                    {
-                        groupId: 999,
-                        title: 'Repeating Event',
-                        start: '2020-09-09T16:00:00'
-                    },
-                    {
-                        groupId: 999,
-                        title: 'Repeating Event',
-                        start: '2020-09-16T16:00:00'
-                    },
-                    {
-                        title: 'Conference',
-                        start: '2020-09-11',
-                        end: '2020-09-13'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2020-09-12T10:30:00',
-                        end: '2020-09-12T12:30:00'
-                    },
-                    {
-                        title: 'Lunch',
-                        start: '2020-09-12T12:00:00'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2020-09-12T14:30:00'
-                    },
-                    {
-                        title: 'Happy Hour',
-                        start: '2020-09-12T17:30:00'
-                    },
-                    {
-                        title: 'Dinner',
-                        start: '2020-09-12T20:00:00'
-                    },
-                    {
-                        title: 'Birthday Party',
-                        start: '2020-09-13T07:00:00'
-                    },
-                    {
-                        title: 'Click for Google',
-                        url: 'http://google.com/',
-                        start: '2020-09-28'
-                    }
-                ]
-            });
+            function montaCalendario() {
+                var d = new Date();
+                var calendarEl = document.getElementById('calendar');
 
-            calendar.render();
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    locale: 'pt-br',
+                    initialDate: d.toISOString(),
+                    editable: true,
+                    selectable: true,
+                    businessHours: true,
+                    dayMaxEvents: true, // allow "more" link when too many events
+                    events: [
+                        {
+                            title: 'All Day Event',
+                            start: '2020-09-01'
+                        },
+                        {
+                            title: 'Outro',
+                            start: '2020-09-01'
+                        },
+                        {
+                            title: 'mais um',
+                            start: '2020-09-01'
+                        },
+                        {
+                            title: 'xxxx',
+                            start: '2020-09-01'
+                        },
+                        {
+                            title: 'Long Event',
+                            start: '2020-09-07',
+                            end: '2020-09-10'
+                        },
+                        {
+                            groupId: 999,
+                            title: 'Repeating Event',
+                            start: '2020-09-09T16:00:00'
+                        },
+                        {
+                            groupId: 999,
+                            title: 'Repeating Event',
+                            start: '2020-09-16T16:00:00'
+                        },
+                        {
+                            title: 'Conference',
+                            start: '2020-09-11',
+                            end: '2020-09-13'
+                        },
+                        {
+                            title: 'Meeting',
+                            start: '2020-09-12T10:30:00',
+                            end: '2020-09-12T12:30:00'
+                        },
+                        {
+                            title: 'Lunch',
+                            start: '2020-09-12T12:00:00'
+                        },
+                        {
+                            title: 'Meeting',
+                            start: '2020-09-12T14:30:00'
+                        },
+                        {
+                            title: 'Happy Hour',
+                            start: '2020-09-12T17:30:00'
+                        },
+                        {
+                            title: 'Dinner',
+                            start: '2020-09-12T20:00:00'
+                        },
+                        {
+                            title: 'Birthday Party',
+                            start: '2020-09-13T07:00:00'
+                        },
+                        {
+                            title: 'Click for Google',
+                            url: 'http://google.com/',
+                            start: '2020-09-28'
+                        }
+                    ]
+                });
+
+                calendar.render();
+            }
         });
     });

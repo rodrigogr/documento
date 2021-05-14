0da84a9da822d84c003aa78a545407d6ce3c847a
Rafael Gonzaga Goncalves <rafael.gonzaga@uzer.com.br>
2021-05-14 12:05:02 -0300

    ajustando migrations

--- api/database/migrations/2021_04_02_203415_add_status_assembleia_questoes_ordens.php
+++ api/database/migrations/2021_04_02_203415_add_status_assembleia_questoes_ordens.php
@@ -15,7 +15,6 @@ class AddStatusAssembleiaQuestoesOrdens extends Migration
     {
         Schema::table('assembleia_questoes_ordens', function (Blueprint $table)
         {
-            $table->enum('status', ['Pendente de decisão de recurso', 'Pendente de decisão', 'Deferida', 'Recurso indeferido', 'Indeferido', 'Recurso deferido'])->default('Pendente de decisão');
             $table->string('status','100')->default('Pendente de decisão');
         });
     }

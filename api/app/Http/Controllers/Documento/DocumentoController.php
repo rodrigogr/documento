9e0370964931014b36ca9b11e6bab4819c25d314
Rodrigo Gonçalves Rebelo <rogrebelo@gmail.com>
2021-06-24 13:45:23 -0300

    Design tela documents e listagem.

--- api/app/Http/Controllers/Documento/DocumentoController.php
+++ api/app/Http/Controllers/Documento/DocumentoController.php
@@ -5,8 +5,13 @@ namespace App\Http\Controllers\Documento;
 
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class DocumentoController extends Controller
 {
-
+    public function index()
+    {
+        $documents = DB::table('documento')->select('nome', 'categoria', 'data_postagem')->get();
+        return response()->success($documents);
+    }
 }
--- site/src/documentos/documentos/documentos.ctrl.js
+++ site/src/documentos/documentos/documentos.ctrl.js
@@ -6,5 +6,12 @@ angular.module('DocumentosModule').controller('DocumentosCtrl',
         let user = JSON.parse(localStorage.getItem("bioacs-uid"));
         AuthService.aclPaginaService($state.$current.name, user.id).then(result => $scope.accessPagina = result.data);
 
+        $scope.listDocument = function () {
+            var promisse = ($http.get(`${config.apiUrl}api/documentos`));
+            promisse.then(function (result) {
+                $scope.listDocuments = result.data.data;
+            });
+        };
+        $scope.listDocument();
 
     });
